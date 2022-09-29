1f1991291e66d7a7dfa30a3b85b8e53362b55222
Stilian Kolev <stiliankolev123@gmail.com>
2022-09-29 09:46:56 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+
+namespace myApp
+{
+    
+    class Program
+    {
+
+        //Command Line Parser
+        public static void Args(string[] args)
+         {
+
+            foreach (string arg in args)
+            {
+                switch (arg) {
+                    case "-h":
+                    case "--help":
+                        Console.WriteLine("Help");
+                        break; 
+                    
+                    case "-l":
+                    case "--list-formats":
+                        Console.WriteLine("CSV, JSON, MD, HTML");
+                        break;
+                    
+                    case "-i":
+                    case "--info":
+                        Console.WriteLine("The version of the currently executing assembly is: {0}", typeof(Program).Assembly.GetName().Version);
+                        break;
+
+                    case "-v":
+                    case "--verbose":
+                        Console.WriteLine("Standard Output Stream: {0}", Console.Out);
+                        //Debug.WriteLine("Debugging Output Stream: {0}", Console.Out);
+                        break;
+
+                    case "-o":
+                    case "--output":
+                        //Console.WriteLine(jsonTable());
+                        Console.WriteLine(checkFile(args));
+                        break;
+                } 
+            } 
+         }
+
+
+        /* public enum tablesFormat
+        {
+            None = 0,
+            JSON = 1
+        } */
+         
+
+         public static string checkFile(string[] args)
+         {
+            string inputTable = "";
+            string fileName = args[0];
+            if (File.Exists(fileName))
+            {
+                inputTable = File.ReadAllText(fileName);
+                return inputTable;
+            }
+            else
+            {
+                Console.WriteLine("File does not exist");
+            }
+            return inputTable;
+         }
+         
+         
+         
+         public static string jsonTable ()
+         {
+            string jsonTableData = File.ReadAllText("table.json");
+            return jsonTableData;
+         }   
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            
+            Program.Args(args);
+        
+            
+        }
+    }
+}
